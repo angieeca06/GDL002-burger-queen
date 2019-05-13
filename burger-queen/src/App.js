8961a1f60fc5f90a7ac9238bd3e0c594cbcec812
angie <angyalva16@gmail.com>
2019-05-13 13:34:31 -0500

    Se creo el componente del navBar

--- burger-queen/src/App.js
+++ burger-queen/src/App.js
@@ -1,30 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from "./Components/Button";
-
-const buttons = [1,2,3,4,5];
+import React, { Component } from 'react';
+import SignUp from "./Components/SignUp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      {buttons.map((button, index) => <Button name={"GDL" + index} color={"background-" + index} key={index}/>)}
-    </div>
-      );
+  return(
+    <SignUp/>
+  );
+  
 };
 
 export default App;
---
+++ burger-queen/src/Components/SignUp.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const navBar = props => {
+    return (
+        <nav>
+            <h2>Burguer Queen</h2>
+            <ul>
+                <li>Meserxs</li>
+                <li>Jefx de meserxs</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default navBar;
