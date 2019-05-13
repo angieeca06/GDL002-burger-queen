2cdae93c338fa97469d9c4d177c33ab4ff3fb71a
unknown <angyalva16@gmail.com>
2019-05-13 11:20:38 -0500

    Cambio de compu

--- burger-queen/src/App.js
+++ burger-queen/src/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from "./Components/Button";
+import SignUp from "./Components/Registro";
 
-const buttons = [1,2,3,4,5];
+// const buttons = [1,2,3];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      {buttons.map((button, index) => <Button name={"GDL" + index} color={"background-" + index} key={index}/>)}
+      <Button name={"Meserxs"} color={"background-red"} key={"meserxs"}/>
+      <Button name={"Jefe de cocina"} color={"background-red"} key={"Jefe de cocina"}/>
     </div>
       );
 };
---
+++ burger-queen/src/Components/Registro.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const signUp = props =>{
+    const action  = props.name;
+
+    return(
+        <input type= {"text"} className={"Name"}></input>
+    );
+};
+
+export default signUp;
